6446470d277c2aefc538e1866fd2142646e59d26
Olli Mämmelä <39191938+ollim84@users.noreply.github.com>
2019-02-01 10:09:56 +0200

    Update DDEAccessPoint.cc

--- Reinforcement Learning for Wireless Access Selection/dde/DDEAccessPoint.cc
+++ Reinforcement Learning for Wireless Access Selection/dde/DDEAccessPoint.cc
@@ -9,7 +9,7 @@
 // but WITHOUT ANY WARRANTY; without even the implied warranty of
 // MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
 // See the GNU Lesser General Public License for more details.
-//
+// Copyright 2013 Olli Mämmelä (VTT)
 
 
 #include "DDEAccessPoint.h"
